0d7c2c8c9174acee800b5a75dbd3909c2349f0fb
Krystian Kozłowski <34834953+krystek97@users.noreply.github.com>
2018-07-30 16:38:05 +0200

    SKYANGULAR
    v 1.2

--- angular-kurs/src/app/app.component.ts
+++ angular-kurs/src/app/app.component.ts
@@ -6,17 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-kurs';
-  colorClass = 'color2' ;
-  logoUrl = 'http://www.drewnianyteatrzyk.pl/environment/cache/images/500_500_productGfx_5408d3e5a551a0e08d115f05a1ff71c6.jpg' ;
-  MaxLength = 6 ;
-  value = 'Wpisz Tekst' ;
-  imageClass = 'angularimage' ;
-
-  change() {
-    this.value = 'Wpisales tekst' ;
-    this.logoUrl = 'https://cygnus.org.pl/wp-content/uploads/2016/12/swan-water-bird-nature-water-57431.jpeg' ;
-  }
+  book = 'Wpisz nazwe ksiazki' ;
+  pages = 'Wpisz ilosc stron' ;
+  saveK(event) {
+    this.book = event.target.value ; }
+  saveP(event) {
+    this.pages = event.target.value ; }
 }
 
 
